5e6cdbcc7ee5491a1273d92dac29a4844adab9bf
Kot Durak <darkprokuror@mail.ru>
2019-06-09 19:35:51 +0400

    reviews done

--- js/main.js
+++ js/main.js
@@ -31,22 +31,63 @@ $(function(){
 	     }
 		]
 	  });
+
+	$(".rew-slick").slick({
+	    dots: false,
+	    infinite: true,
+	    slidesToShow: 2,
+	    slidesToScroll: 1,
+	     cssEase: 'linear',
+	     responsive: [
+	     {
+	     	breakpoint: 1024,
+	     	settings: {
+	     		slidesToShow: 2,
+	     		slidesToScroll: 2,
+	     		infinite: true,
+	     		dots: true
+	     	}
+	     },
+	     {
+	     	breakpoint: 600,
+	     	settings: {
+	     		slidesToShow: 2,
+	     		slidesToScroll: 2
+	     	}
+	     },
+	     {
+	     	breakpoint: 480,
+	     	settings: {
+	     		slidesToShow: 1,
+	     		slidesToScroll: 1
+	     	}
+	     }
+		]
+	  });
+
 	var D_custom_nav = $('.custom-nav');	
+	var no_hide = true; 
+
 	$('.main-catalog').on('click', function(event){
 		$('.custom-nav').toggle();
 	});
 
 	$(window).on('scroll', function(event){
-		D_custom_nav.hide();
+		if(!no_hide){
+			D_custom_nav.hide();
+		}
+		
 	});
 
 	$('.catalog').on('mouseover', function(event){
 		var D_elem = $(this);
+		no_hide = true;
 		D_elem.find('.dropdown-menu').show();
 	});
 
 	$('.catalog').on('mouseout', function(event){
 		var D_elem = $(this);
+		no_hide = false;
 		D_elem.find('.dropdown-menu').hide();
 	});
 });
